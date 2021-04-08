093f936cbddb96f5f2eac9a1c3ada57cf74736e3
Stephen Flynn <25164860+syntaxerrorStephen@users.noreply.github.com>
2021-04-08 16:46:39 +0100

    Create assignmentOp.php

---
+++ PHP/assignmentOp.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Title</title>
+</head>
+
+<body>
+
+
+<?php
+
+    //Assignement Operators
+    $x = 100;
+    $x += 20;
+
+
+
+    echo $x;
+
+?>
+
+</body>
+
+</html>
