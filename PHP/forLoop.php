cccf14171958e73aecb55783a8eb251e7be564d9
Stephen Flynn <25164860+syntaxerrorStephen@users.noreply.github.com>
2021-04-08 18:08:09 +0100

    Create forLoop.php

---
+++ PHP/forLoop.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Title</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+
+
+<?php
+    //For Loop
+    
+    for ($x = 1; $x <= 10; $x++) {
+        echo "Hello<br>";
+
+    }
+?>
+
+</body>
+
+</html>
