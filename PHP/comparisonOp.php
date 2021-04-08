ebbb117f1149b9eda06a4aad1524f3f4602d11e5
Stephen Flynn <25164860+syntaxerrorStephen@users.noreply.github.com>
2021-04-08 16:47:35 +0100

    Create comparisonOp.php

---
+++ PHP/comparisonOp.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Title</title>
+</head>
+
+<body>
+
+
+<?php
+
+    //Comparison Operators
+    $x = 5;
+    $y = 10;
+
+    if ($x === $y) {
+        echo "True!";
+    }
+    else {
+        echo "False!";
+    }
+?>
+
+</body>
+
+</html>
