20768e711142acc2b4147598b444feafa2fd7fb2
Stephen Flynn <25164860+syntaxerrorStephen@users.noreply.github.com>
2021-04-08 16:51:06 +0100

    Create statements.php

---
+++ PHP/statements.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Title</title>
+</head>
+
+<body>
+
+
+<?php
+
+    //Statements
+    $x = 1;
+    if ($x == 1) {
+        echo "Hello";
+    } 
+    
+    elseif ($x == 2) {
+        echo "Morning";
+    }
+    
+    else {
+        echo "Goodbye";
+    }
+   
+    
+?>
+
+</body>
+
+</html>
