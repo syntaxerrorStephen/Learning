2a60d550b971da52e594e88f35400ce550b852ad
Stephen Flynn <25164860+syntaxerrorStephen@users.noreply.github.com>
2021-04-09 00:32:34 +0100

    Add files via upload

---
+++ PHP/signUp/includes/signup.inc.php
@@ -0,0 +1,17 @@
+<?php
+
+    include_once 'dbh.inc.php';
+
+    $first = mysqli_real_escape_string($conn, $_POST['first']);
+    $last = mysqli_real_escape_string($conn, $_POST['last']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $uid = mysqli_real_escape_string($conn, $_POST['uid']);
+    $pwd = mysqli_real_escape_string($conn, $_POST['pwd']);
+
+    $sql = "INSERT INTO users (user_first, user_last, user_email, user_uid, user_pwd)
+    VALUES ('$first', '$last', '$email', '$uid', '$pwd');";
+    mysqli_query($conn, $sql);
+
+    HEADER("Location: ../index.php?signup=success");
+
+?>
