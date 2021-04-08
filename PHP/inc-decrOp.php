3ea672ddb881354f5bbfb39cf334fa723ec671b9
Stephen Flynn <25164860+syntaxerrorStephen@users.noreply.github.com>
2021-04-08 16:49:51 +0100

    Create inc-decrOp.php

---
+++ PHP/inc-decrOp.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Title</title>
+</head>
+
+<body>
+
+
+<?php
+
+    //Increment/Decrement Operators
+    $x = 10;
+    echo ++$x;
+    echo --$x;
+?>
+
+</body>
+
+</html>
