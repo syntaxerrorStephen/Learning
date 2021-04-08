631dbffee4893da1525557c8ac4d8b8ab352dff7
Stephen Flynn <25164860+syntaxerrorStephen@users.noreply.github.com>
2021-04-08 19:01:52 +0100

    Add files via upload

---
+++ PHP/includeFile/contact.php
@@ -0,0 +1,15 @@
+<?php
+    include 'header.php';
+?>
+
+
+<section>
+    <div class="someMainContent">
+        <h1>Hello there</h1>
+    </div>
+</section>
+
+
+</body>
+
+</html>
---
+++ PHP/includeFile/header.php
@@ -0,0 +1,25 @@
+<?php
+    include 'functions/user-functions.php';
+?>
+
+<!DOCTYPE html>
+
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Title</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+
+<header>
+    <nav>
+        <ul>
+            <li><a href="">nav1</a></li>
+            <li><a href="">nav2</a></li>
+            <li><a href="">nav3</a></li>
+            <li><a href="">nav4</a></li>
+        </ul>
+    </nav>
+</header>
