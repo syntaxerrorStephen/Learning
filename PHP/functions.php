27bc1035ae9616c42bce41776ecec370f0ab263a
Stephen Flynn <25164860+syntaxerrorStephen@users.noreply.github.com>
2021-04-08 16:49:28 +0100

    Create functions.php

---
+++ PHP/functions.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Title</title>
+</head>
+
+<body>
+
+
+<?php
+
+    //echo strlen("Hi Stephen");
+    //echo str_word_count("Hi Stephen");
+    //echo strrev("Hi Stephen");
+    //echo strpos("Hi Stephen", "Hi");
+    //echo str_replace("Stephen","Steve","Hi Stephen");
+
+?>
+
+</body>
+
+</html>
