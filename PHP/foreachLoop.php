a883e853541be64714ba0b5f7ed66ae4b728946d
Stephen Flynn <25164860+syntaxerrorStephen@users.noreply.github.com>
2021-04-08 18:12:26 +0100

    Create foreachLoop.php

---
+++ PHP/foreachLoop.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Title</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+
+
+<?php
+    //Foreach Loop
+    
+    $array = array("Stephen", "Steve", "Steven");
+
+    foreach ($array as $loopdata) {
+        echo "My name is ".$loopdata."<br>";
+    }
+?>
+
+</body>
+
+</html>
