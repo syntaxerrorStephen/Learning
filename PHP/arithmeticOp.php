add19dcfb30c1ae7d80ff633bc20a13a157c7d73
Stephen Flynn <25164860+syntaxerrorStephen@users.noreply.github.com>
2021-04-08 16:46:09 +0100

    Create arithmeticOp.php

---
+++ PHP/arithmeticOp.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Title</title>
+</head>
+
+<body>
+
+
+<?php
+
+    //Arithmetic Operators
+    echo 2**5;
+    echo 2/5;
+    echo 2*5;
+    echo 2+5;
+    echo 2%5;
+
+?>
+
+</body>
+
+</html>
