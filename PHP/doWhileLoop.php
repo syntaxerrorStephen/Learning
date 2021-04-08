c0bdaae68b08642531c3aa2a966a02c3b058010c
Stephen Flynn <25164860+syntaxerrorStephen@users.noreply.github.com>
2021-04-08 18:01:51 +0100

    Create doWhileLoop.php

---
+++ PHP/doWhileLoop.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Title</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+
+
+<?php
+    //Do While Loop
+    
+    $x = 4;
+    do {
+        echo "Hello there<br>";
+        $x++;
+    }
+    while($x <= 5);
+?>
+
+</body>
+
+</html>
