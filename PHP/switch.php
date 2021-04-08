7cba5bf4764ab552cabf35747597e3a61dcbe16b
Stephen Flynn <25164860+syntaxerrorStephen@users.noreply.github.com>
2021-04-08 16:51:31 +0100

    Create switch.php

---
+++ PHP/switch.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Title</title>
+</head>
+
+<body>
+
+
+<?php
+
+    //Switch
+    $x = 1;
+    switch ($x) {
+        case 1:
+            echo "The answer is 1";
+        break;
+        case 2:
+            echo "The answer is 2";
+        case 3:
+            echo "The answer is 3";
+        break;
+        case 4:
+            echo "The answer is 4";
+        break;
+        default:
+        echo "There is no answer";
+    }
+   
+    
+?>
+
+</body>
+
+</html>
