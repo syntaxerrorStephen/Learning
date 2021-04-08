ab34b810603256a3fa866990301dfd3b1628f563
Stephen Flynn <25164860+syntaxerrorStephen@users.noreply.github.com>
2021-04-08 16:48:02 +0100

    Create dataTypes.php

---
+++ PHP/dataTypes.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Title</title>
+</head>
+
+<body>
+
+
+<?php
+
+    //String
+    $name = 'Just a text'
+
+    //Integer
+    $name = 20;
+
+    //Float
+    $name = 20.7654;
+
+    //Boolean
+    true = 1
+    false = 0
+
+    //Array
+    $names = array("Stephen","Steve","Steven");
+    echo $names['0']
+
+?>
+
+</body>
+
+</html>
