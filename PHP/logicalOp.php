b92cbac5204ebd47e2842c0ca19f628fb0a152df
Stephen Flynn <25164860+syntaxerrorStephen@users.noreply.github.com>
2021-04-08 16:50:23 +0100

    Create logicalOp.php

---
+++ PHP/logicalOp.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Title</title>
+</head>
+
+<body>
+
+
+<?php
+
+    //Logical Operators
+    $x = 10;
+    $y = 20;
+
+    if ($x == $y || 1 == 1) {
+        echo "True";
+    }
+
+    if ($x == $y && 1 == 1) {
+        echo "True";
+    }
+
+    if ($x == $y xor 1 == 1) {
+        echo "True";
+    }
+    
+?>
+
+</body>
+
+</html>
