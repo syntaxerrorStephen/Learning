43a8ed2369a5c59b089446947338f8e424158f81
Stephen Flynn <25164860+syntaxerrorStephen@users.noreply.github.com>
2021-04-08 18:16:18 +0100

    Create arrays.php

---
+++ PHP/arrays.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Title</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+
+
+<?php
+    //Arrays
+    
+    $array = array("Stephen", "Steve", "Steven");
+
+    echo $array[2];
+?>
+
+</body>
+
+</html>
