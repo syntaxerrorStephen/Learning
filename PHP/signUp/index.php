55834480f651364e26987f5c5fc53ed69f8e107c
Stephen Flynn <25164860+syntaxerrorStephen@users.noreply.github.com>
2021-04-09 00:31:31 +0100

    Add files via upload

---
+++ PHP/signUp/index.php
@@ -0,0 +1,27 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+
+<form action="includes/signup.inc.php" method="POST">
+    <input type="text" name="first" placeholder="Firstname">
+    <br>
+    <input type="text" name="last" placeholder="Lastname">
+    <br>
+    <input type="text" name="email" placeholder="E-Mail">
+    <br>
+    <input type="text" name="uid" placeholder="Username">
+    <br>
+    <input type="password" name="pwd" placeholder="Password">
+    <br>
+    <button type="submit" name="submit">Sign Up</button>
+</form>
+
+
+
+</body>
+</html>
